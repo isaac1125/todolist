59845ebc76824e94ecc313b89dabe198ef18e88f
isaac1125 <desire82023@gmail.com>
2021-06-21 01:43:29 +0800

    Update all.js

--- all.js
+++ all.js
@@ -1,18 +1,36 @@
-let data = [
-    {
-        "content": "buy gift&cake"
-    },
-    {
-        "content": "finance report convention"
-    }
-]
+const txt = document.querySelector(".txt");
+const save = document.querySelector(".save");
+const list = document.querySelector(".list");
+
+let data = [];
 
 function renderData() {
     let str = "";
-    data.forEach(function (item) {
-        str += `<li>${item.content}<input type="button" value="刪除代辦" class="delete"></li>`
+    data.forEach(function (item, index) {
+        str += `<li>${item.content}<input type="button" value="刪除代辦" data-num="${index}" class="delete"></li>`
     })
     const list = document.querySelector(".list");
     list.innerHTML = str;
 }
-renderData();
+
+save.addEventListener("click", function (e) {
+    if (txt.value == "") {
+        alert("請輸入內容");
+        return;
+    }
+    obj = {};
+    obj.content = txt.value;
+    data.push(obj);
+    renderData();
+    txt.value = "";
+})
+
+list.addEventListener("click", function (e) {
+    if (e.target.getAttribute("class") !== "delete") {
+        alert("請點按鈕喔")
+        return;
+    }
+    let num = e.target.getAttribute("data-num");
+    data.splice(num, 1);
+    renderData();
+})
